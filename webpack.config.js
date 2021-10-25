4499c86657f75c888a0498d5c87b529eb9db06d2
Aryan Deora <adeora@iu.edu>
2021-10-24 23:32:50 -0400

    Webpack Config Change

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   target: 'node',
   entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname + "/dist"),
+    path: path.resolve(__dirname + '/dist'),
     filename: 'hyper-dynamic-wallpaper.js',
-    libraryTarget: 'commonjs'
+    libraryTarget: 'commonjs',
   },
   optimization: {
     minimize: true,
     minimizer: [
-        new TerserPlugin({
-            terserOptions: {
-                keep_classnames: true,
-                keep_fnames: true
-            }
-          })
-        ]
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+      }),
+    ],
   },
-  plugins: [ new webpack.DefinePlugin({ 'global.GENTLY': false }) ],
-  externals: [ nodeExternals(), 'hyper/component', 'hyper/notify', 'hyper/decorate' ],
+  // plugins: [ new webpack.DefinePlugin({ 'global.GENTLY': false }) ],
+  externals: [nodeExternals(), 'hyper/component', 'hyper/notify', 'hyper/decorate'],
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-}
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
